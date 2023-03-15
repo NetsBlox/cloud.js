f83ddd8900b4ec9705256a9e90196fa4c406b85b
Brian Broll <brian.broll@gmail.com>
2023-03-15 16:31:50 -0500

    Fix warnings with build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,26 @@
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
-const plugins = [commonjs(), nodeResolve(), typescript()];
+const plugins = [
+  commonjs(),
+  nodeResolve({
+    preferBuiltins: true,
+  }),
+  json(),
+  typescript(),
+];
 
 export default [
   {
-    input: 'src/client.ts',
+    input: "src/client.ts",
     output: {
-      file: 'dist/client.js',
-      format: 'umd',
-      name: 'Cloud'
+      file: "dist/client.js",
+      format: "umd",
+      name: "Cloud",
+      exports: "named",
     },
     plugins,
-    preferBuiltins: true,
   },
 ];
