7c1a8103efc164660404471b4f6fd4978871abb9
Brian Broll <brian.broll@gmail.com>
2023-03-16 09:31:48 -0500

    Remove unnecessary rollup plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,8 @@
 import typescript from "@rollup/plugin-typescript";
-import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
 
 const plugins = [
   commonjs(),
-  nodeResolve({
-    preferBuiltins: true,
-  }),
-  json(),
   typescript(),
 ];
 
